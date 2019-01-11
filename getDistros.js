5a2eec5a1a195985964274765135463ccbcfa062
Darren <ninenumbproductions@gmail.com>
2019-01-11 15:07:58 -0800

    Slice distroList into 5 groups of 20

--- getDistros.js
+++ getDistros.js
@@ -3,6 +3,11 @@ let cheerio = require('cheerio');
 let fs = require('fs');
 
 const distroList = [];
+let d0_20;
+let d21_40;
+let d41_60;
+let d61_80;
+let d81_100;
 
 axios.get('https://distrowatch.com/index.php?dataspan=1')
   .then((response) => {
@@ -16,6 +21,21 @@ axios.get('https://distrowatch.com/index.php?dataspan=1')
         // console.log(a.text());
         distroList.push(a.text());
       });
-      console.log(distroList);     
+      d0_20 = distroList.slice(0, 20);
+      // console.log(d0_20);
+      d21_40 = distroList.slice(20, 40);
+      // console.log(d21_40);      
+      d41_60 = distroList.slice(40, 60);
+      // console.log(d41_60);
+      d61_80 = distroList.slice(60, 80);
+      // console.log(d61_80);
+      d81_100 = distroList.slice(80, 100);
+      // console.log(d81_100);
+
+      console.log(distroList);
+
+
+
+
     }
-  }, (error) => console.log('error: ', err));
+  }, (error) => console.log('error: ', err))
