50fae5e66dce9d2895e9e49fd2af0990450ec788
Darren <ninenumbproductions@gmail.com>
2019-01-19 23:01:33 -0800

    getBracketValue created, cleanup variable names

--- controller.js
+++ controller.js
@@ -1,13 +1,11 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
+const getBracketRange = require('./getBracketValue');
 
 exports.getDistros = (req, res) => {
   const distroList = [];
-  const rootURL = 'https://distrowatch.com/table.php?distribution=';
-  let rankBracket;
-
-  const randomRank = Math.floor(Math.random() * 20);
-  const oneToOneHundred = Math.floor(Math.random() * 100 + 1);
+  const bracketRange = getBracketRange(Math.floor(Math.random() * 100 + 1));
+  const randomIndex = Math.floor(Math.random() * 20);  
 
   return axios.get('https://distrowatch.com/index.php?dataspan=1')
     .then((response) => {
@@ -15,33 +13,24 @@ exports.getDistros = (req, res) => {
         const html = response.data;
         const $ = cheerio.load(html);
 
+        //find distro list and for each one create {distro, query, rank}
         $('.phr2').each(function (i, element) {
           let a = $(this).children();
           const distro = a.text();
           const href = a['0'].attribs.href; //ex: "mx?frphr">
           const query = href.substr(0, href.indexOf('?'));
-
           distroList.push({ distro, query, rank: i + 1 });
         });
 
-        if (oneToOneHundred <= 45) {
-          rankBracket = distroList.slice(0, 20);
-        } else if (oneToOneHundred >= 46 && oneToOneHundred <= 70) {
-          rankBracket = distroList.slice(20, 40);
-        } else if (oneToOneHundred >= 71 && oneToOneHundred <= 85) {
-          rankBracket = distroList.slice(40, 60);
-        } else if (oneToOneHundred >= 86 && oneToOneHundred <= 95) {
-          rankBracket = distroList.slice(60, 80);
-        } else {
-          rankBracket = distroList.slice(80, 100);
-        }
-        console.log('Random 1 - 100 num: ',oneToOneHundred);   
+        distroToInstall = distroList.slice(bracketRange.min, bracketRange.max)[randomIndex];
+        
+        console.log('bracket Range:', bracketRange.min, '-', bracketRange.max);   
       }
 
       res.render('layout', {
-        distro: rankBracket[randomRank].distro,
-        url: rootURL + rankBracket[randomRank].query,
-        rank: rankBracket[randomRank].rank
+        distro: distroToInstall.distro,
+        url: 'https://distrowatch.com/table.php?distribution=' + distroToInstall.query,
+        rank: distroToInstall.rank
       });
 
     })
