24cabb6368c4e1b5a939671b8e0d5bce21ef84a8
Darren <ninenumbproductions@gmail.com>
2019-01-19 22:13:29 -0800

    will render random distro weighted for rank

--- controller.js
+++ controller.js
@@ -1,55 +1,49 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
-// let fs = require('fs');
 
 exports.getDistros = (req, res) => {
   const distroList = [];
   const rootURL = 'https://distrowatch.com/table.php?distribution=';
-  let d0_20;
-  let d21_40;
-  let d41_60;
-  let d61_80;
-  let d81_100;
+  let rankBracket;
+
+  const randomRank = Math.floor(Math.random() * 20);
+  const oneToOneHundred = Math.floor(Math.random() * 100 + 1);
 
   return axios.get('https://distrowatch.com/index.php?dataspan=1')
     .then((response) => {
-      if (response.status === 200) {        
+      if (response.status === 200) {
         const html = response.data;
         const $ = cheerio.load(html);
 
         $('.phr2').each(function (i, element) {
-          var a = $(this).children();
-          // console.log(a.text());
-          // distroList.push(a);
+          let a = $(this).children();
           const distro = a.text();
-
           const href = a['0'].attribs.href; //ex: "mx?frphr">
           const query = href.substr(0, href.indexOf('?'));
-          distroList.push({distro , query, rank: i + 1});
+
+          distroList.push({ distro, query, rank: i + 1 });
         });
 
-        d0_20 = distroList.slice(0, 20);
-        // console.log(d0_20);
-        d21_40 = distroList.slice(20, 40);
-        // console.log(d21_40);      
-        d41_60 = distroList.slice(40, 60);
-        // console.log(d41_60);
-        d61_80 = distroList.slice(60, 80);
-        // console.log(d61_80);
-        d81_100 = distroList.slice(80, 100);
-        // console.log(d81_100);
-
-        console.log(distroList);
-        
-
-        // const href = distroList[1]['0'].attribs.href;
-        // console.log(href); //ex mx?frphr        
-
-        // const query = href.substr(0, href.indexOf('?'));
-        // console.log(query);
+        if (oneToOneHundred <= 45) {
+          rankBracket = distroList.slice(0, 20);
+        } else if (oneToOneHundred >= 46 && oneToOneHundred <= 70) {
+          rankBracket = distroList.slice(20, 40);
+        } else if (oneToOneHundred >= 71 && oneToOneHundred <= 85) {
+          rankBracket = distroList.slice(40, 60);
+        } else if (oneToOneHundred >= 86 && oneToOneHundred <= 95) {
+          rankBracket = distroList.slice(60, 80);
+        } else {
+          rankBracket = distroList.slice(80, 100);
+        }
+        console.log('Random 1 - 100 num: ',oneToOneHundred);   
       }
-      res.render('layout', { distro: distroList[0].distro, url: rootURL + distroList[0].query  });
+
+      res.render('layout', {
+        distro: rankBracket[randomRank].distro,
+        url: rootURL + rankBracket[randomRank].query,
+        rank: rankBracket[randomRank].rank
+      });
+
     })
-    .catch(err => console.log('There was a problem: ', err)
-    )
+    .catch(err => console.log('There was a problem: ', err))
 }
